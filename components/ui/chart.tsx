5be2858f4299fa54ba36f106a10e4e50de080e52
manthan <manthan.jsharma@gmail.com>
2025-04-04 19:43:00 +0530

    recharts updates

---
+++ components/ui/chart.tsx
@@ -0,0 +1,33 @@
+import {
+  AreaChart as RechartsAreaChart,
+  Area as RechartsArea,
+  BarChart as RechartsBarChart,
+  Bar as RechartsBar,
+  CartesianGrid as RechartsCartesianGrid,
+  Legend as RechartsLegend,
+  LineChart as RechartsLineChart,
+  Line as RechartsLine,
+  PieChart as RechartsPieChart,
+  Pie as RechartsPie,
+  ResponsiveContainer as RechartsResponsiveContainer,
+  Tooltip as RechartsTooltip,
+  XAxis as RechartsXAxis,
+  YAxis as RechartsYAxis,
+  Cell as RechartsCell,
+} from "recharts";
+
+export const Area = RechartsArea;
+export const AreaChart = RechartsAreaChart;
+export const Bar = RechartsBar;
+export const BarChart = RechartsBarChart;
+export const CartesianGrid = RechartsCartesianGrid;
+export const Legend = RechartsLegend;
+export const Line = RechartsLine;
+export const LineChart = RechartsLineChart;
+export const Pie = RechartsPie;
+export const PieChart = RechartsPieChart;
+export const ResponsiveContainer = RechartsResponsiveContainer;
+export const Tooltip = RechartsTooltip;
+export const XAxis = RechartsXAxis;
+export const YAxis = RechartsYAxis;
+export const Cell = RechartsCell;
